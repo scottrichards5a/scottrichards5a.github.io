dbf070cc4bf728428e59e05a9a627615026fbd1e
Scott Richards <scott@droptrip.com>
2018-11-10 11:21:10 -0500

    adjusting 1892 bounds and partitioning javascript

--- assets/js/interactiveTimeline.js
+++
@@ -1,8 +0,0 @@
-function selectDate(hashDate) {
-  $("ul#dates>li>div.selected").removeClass("selected")
-  $("ul#dates div:has(a[href='" + hashDate + "'])").addClass("selected")
-  var overlayData = dateOverlays.get(parseInt(hashDate.replace("#", "")))
-  if (overlayData && overlayData.img) {
-    overlay = new ImageOverlay(overlayData.bounds, overlayData.img, map)
-  }
-}
