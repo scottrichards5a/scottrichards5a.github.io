579fdc4ac9af02f6c45bba4ada69ab8634907281
Scott Richards <scott@droptrip.com>
2018-11-10 19:29:23 -0500

    adjusting 1892 bounds

--- assets/js/interactiveTimeline.js
+++ assets/js/interactiveTimeline.js
@@ -6,8 +6,8 @@ dateOverlays.set(1868, {"img": "", "bounds": new google.maps.LatLngBounds(
   )})
 dateOverlays.set(1892, 
   {"img":"/assets/img/Manchester 1892.png", "bounds": new google.maps.LatLngBounds(
-    new google.maps.LatLng(42.981055, -71.470185),
-    new google.maps.LatLng(43.000575, -71.459155)
+    new google.maps.LatLng(42.989805, -71.470175),
+    new google.maps.LatLng(43.000575, -71.459165)
   )});
 dateOverlays.set(1966, {"img": "", "bounds": new google.maps.LatLngBounds(
     new google.maps.LatLng(42.98235, -71.47152),
